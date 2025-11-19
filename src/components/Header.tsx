1643ad956d1916677f94d33fa8a2676cb25e9940
lfguerra1 <luizfelipeguerra09@gmail.com>
2025-11-18 22:15:25 -0300

    fix: alterando imagem logo

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,7 @@
 import { useState } from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import { Menu, X } from "lucide-react";
-import logoTeste from "/logo-teste.png";
+import logoCeub from "/logo-ceub.png";
 import { useAuth } from "../hooks/useAuth";
 
 const Header = () => {
@@ -26,7 +26,7 @@ const Header = () => {
     <header className="bg-[linear-gradient(to_right,_#9A238B_51%,_#340C2F_100%)] text-white w-full px-4 py-3">
       <div className="flex justify-between items-center max-w-5xl mx-auto px-2">
         <div className="flex items-center gap-4">
-          <img src={logoTeste} alt="CEUB Logo" className="h-12 w-auto" />
+          <img src={logoCeub} alt="CEUB Logo" className="h-12 w-auto" />
           <h1 className="text-white text-xl md:text-2xl font-semibold">
             Núcleo de Esportes
           </h1>
